2e60c9e7d441fead417b9f11b11b823603919aee
dino941 <fineart13941@gmail.com>
2026-08-10 21:39:05 +0530

    Medium 1 ms (40.73%) 61.3 MB (55.79%) 08-10-2026 0189-rotate-array .java

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] arr, int k) {
+        int l=0;
+        int r=arr.length-1;
+        k=k%(r+1);
+        revers(arr,l,r);
+        revers(arr,l,k-1);
+        revers(arr,k,r);
+    }
+    public void revers(int[] arr,int l,int r){
+
+        while(l<=r){
+            int tmp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=tmp;
+            r--;
+            l++;
+        }
+    }
+}
