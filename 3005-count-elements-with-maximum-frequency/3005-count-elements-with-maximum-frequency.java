e3586e0043974baf8196a1d0f7b6dedf3e0a15b4
dino941 <fineart13941@gmail.com>
2026-08-14 09:38:31 +0530

    Easy 2 ms (71.51%) 43.4 MB (98.89%) 08-14-2026 3005-count-elements-with-maximum-frequency .java

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -5,12 +5,12 @@ class Solution {
             mp.put(n,mp.getOrDefault(n,0)+1);
         }
         int max=-1;
-        for(int n:mp.keySet()){
-            max=Math.max(max,mp.get(n));
+        for(int n:mp.values()){
+            max=Math.max(max,n);
         }
         int c=0;
-        for(int n:mp.keySet()){
-            if(mp.get(n)==max){
+        for(int n:mp.values()){
+            if(n==max){
                 c+=max;
             }
         }
