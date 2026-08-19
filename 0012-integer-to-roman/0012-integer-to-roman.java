b4976792eed25637c164d931c041e1d58dd4d979
dino941 <fineart13941@gmail.com>
2026-08-19 19:01:23 +0530

    Medium 3 ms (99.82%) 46 MB (95.87%) 08-19-2026 0012-integer-to-roman .java

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String intToRoman(int num) {
+        int arr[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] s={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<arr.length && num>0;i++){
+            while(num>=arr[i]){
+            num-=arr[i];
+            sb.append(s[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
