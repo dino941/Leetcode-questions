cfeb3d27a1bba21e780e4f1bcbc7383195f982b8
dino941 <fineart13941@gmail.com>
2026-08-24 15:23:26 +0530

    Hard 4 ms (66.81%) 86.7 MB (72.34%) 08-24-2026 1872-stone-game-viii .java

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int stoneGameVIII(int[] arr) {
+        int n=arr.length;
+        int prefix[]=new int[n];
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        } 
+        int dp[] =new int[n];
+        dp[n-1]=prefix[n-1];
+        for(int i=n-2;i>0;i--){
+            dp[i]=Math.max(dp[i+1],prefix[i]-dp[i+1]);
+        }
+        return dp[1];
+
+    }
+}
