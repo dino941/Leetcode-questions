e9389bd0ca3f77875d65eee06af0efd581a148b3
dino941 <fineart13941@gmail.com>
2026-08-26 16:05:46 +0530

    Medium 4 ms (97.44%) 47.8 MB (51.43%) 08-26-2026 0658-find-k-closest-elements .java

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int l=0,r=arr.length-1;
         List<Integer> lst=new ArrayList<>();
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
-           int dif=Math.abs(b-x)-Math.abs(a-x);
-           if(dif==0) return b-a;
-           return dif;
-        });
-        for(int i=0;i<arr.length;i++){
-            pq.offer(arr[i]);
-            lst.add(arr[i]);
-            if(pq.size()>k){
-                lst.remove(pq.poll());
+        while(r-l>=k){
+            if(Math.abs(arr[l]-x)>Math.abs(arr[r]-x)){
+                l++;
+            }else{
+                r--;
             }
+        }
+        for(int i=l;i<=r;i++){
+            lst.add(arr[i]);
         }
         return lst;
     }
